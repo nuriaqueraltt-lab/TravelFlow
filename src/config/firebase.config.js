bb64e8c305e0c9e5d421e5b3fadfd290d0a27858
nuriaqueraltt-lab <nuria.queraltt@gmail.com>
2026-07-14 15:30:27 +0200

    feat: add Firebase web configuration

---
+++ src/config/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyABzw9b6cCdQLwSs-P8cb9Y6mBINe2SjWI",
+  authDomain: "travelflow-e1bde.firebaseapp.com",
+  projectId: "travelflow-e1bde",
+  storageBucket: "travelflow-e1bde.firebasestorage.app",
+  messagingSenderId: "486120186124",
+  appId: "1:486120186124:web:4ac06cdbaf941ecfd36929"
+};
