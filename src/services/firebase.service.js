015c119e1ad4196ff37b5a5406ad93941ca4ba9a
nuriaqueraltt-lab <nuria.queraltt@gmail.com>
2026-07-14 15:30:47 +0200

    feat: initialize Firebase services

---
+++ src/services/firebase.service.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+
+import { firebaseConfig } from "../config/firebase.config.js";
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
+export const storage = getStorage(firebaseApp);
